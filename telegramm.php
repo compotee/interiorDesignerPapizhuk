67187c2565063fa40ad83ac17f4d853eca7b7337
Марина <mp795610@gmail.com>
2023-12-22 16:38:22 +0500

    /

--- telegramm.php
+++ telegramm.php
@@ -17,23 +17,23 @@ foreach($arr as $key => $value) {
 $token = "6919275116:AAGXuMO9DSm17HKXOXrhmThqhRhdLKcO-iI";
 $chat_id = "-4025228245";
 
-// $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 // URL для отправки сообщения в Telegram
-$telegram_url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=" . urlencode($txt);
+// $telegram_url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=" . urlencode($txt);
 
 // Отправляем запрос к API Telegram
-$response = file_get_contents($telegram_url);
+// $response = file_get_contents($telegram_url);
 
-if ($response === false) {
-    echo 'Ошибка при отправке заявки.';
-} else {
-    echo 'Заявка успешно отправлена.';
-}
-
-
-// if ($sendToTelegram) {
-//   echo "Good!";
+// if ($response === false) {
+//     echo 'Ошибка при отправке заявки.';
 // } else {
-//   echo "Error";
-// }t 
+//     echo 'Заявка успешно отправлена.';
+// }
+
+
+if ($sendToTelegram) {
+  echo "Good!";
+} else {
+  echo "Error";
+} 
