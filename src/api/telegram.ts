428f98c34288d05df468857da7ae73a4eac371bc
EkaterinaKovaleva <kovaleva.ekaterina03@mail.ru>
2023-12-22 17:17:21 +0500

    new

---
+++ src/api/telegram.ts
@@ -0,0 +1,8 @@
+//  const baseUrl = 'https://api.telegram.org/bot6708606839:AAFPUf0zjK31txAcZ_-GYHEt67L_Vng6sWQ/'
+ 
+//  export const sendMessage = async (messageM: string): Promise<void> =>{
+//     const url : string = `${baseUrl}sendMessage?chat_id=-4003897361text=${messageM};`
+
+//     const response : Response = await fetch(url);
+
+//  }
